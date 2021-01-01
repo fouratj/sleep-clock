e3fd2f692df5c572bcb87ba7694b1ef31c1fba73
Fourat Janabi <fouratj@gmail.com>
2021-01-01 15:09:25 +1100

    update tab types

--- types.tsx
+++ types.tsx
@@ -4,8 +4,8 @@ export type RootStackParamList = {
 };
 
 export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+  Sleep: undefined;
+  Settings: undefined;
 };
 
 export type TabOneParamList = {
