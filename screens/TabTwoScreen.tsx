ff010f8f1a64c3d286075d67a62f96a8f4aef3d7
Fourat Janabi <fouratj@gmail.com>
2021-01-02 16:33:18 +1100

    Add settings

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,24 +1,62 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import { ListItem, Avatar, Overlay } from 'react-native-elements';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import Picker from '../components/Picker';
+import Alarm from '../state/alarm';
+
+const list = [
+  {
+    name: 'Sleep Time',
+    avatar_url:
+      'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Time you would like to sleep for',
+    onPress: (toggle: () => void) => toggle(),
+  },
+];
 
 export default function TabTwoScreen() {
+  const [visible, setVisible] = React.useState(false);
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+
+  const onChange = (a) => {
+    Alarm.setDuration(a);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+    <View>
+      {list.map((l, i) => (
+        <ListItem key={l.name} bottomDivider onPress={toggleOverlay}>
+          <Avatar source={{ uri: l.avatar_url }} />
+          <ListItem.Content>
+            <ListItem.Title>{l.name}</ListItem.Title>
+            <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+          </ListItem.Content>
+        </ListItem>
+      ))}
+
+      <Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
+        <View style={styles.container}>
+          <Picker onSelected={onChange} />
+        </View>
+      </Overlay>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'white',
+    width: 250,
+    height: 250,
   },
   title: {
     fontSize: 20,
