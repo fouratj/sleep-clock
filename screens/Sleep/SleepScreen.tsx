e1e248d9e2b245773f659da87ac37ced279d0052
Fourat Janabi <fouratj@gmail.com>
2021-01-10 17:31:08 +1100

    Update SleepScreen

--- screens/Sleep/SleepScreen.tsx
+++ screens/Sleep/SleepScreen.tsx
@@ -1,135 +1,54 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
 import { Audio } from 'expo-av';
-import dayjs from 'dayjs';
 import { observer } from 'mobx-react';
-import { interval } from 'rxjs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Button as RNButton } from 'react-native-elements';
 
 import { Text, View } from '../../components/Themed';
-import Button from '../../components/Button';
-import Alarm, { AlarmStatus } from '../../state/alarm';
 import Top from './components/Top';
 import Bottom from './components/Bottom';
 
-const countdown$ = interval(1000); // 1 min
-
-export default observer(() => {
-  const [left, setLeft] = React.useState<string | undefined>('');
-  const timer = React.useRef<number>();
+export default function SleepScreen() {
+  const [loading, setLoading] = React.useState(true);
   const soundRef = React.useRef<Audio.Sound>();
-  const endTime = Alarm.endTime;
 
   React.useEffect(() => {
+    setLoading(true);
+
     Audio.Sound.createAsync(require('../../assets/audio/alarm.wav')).then(
       ({ sound }) => {
         soundRef.current = sound;
+        setLoading(false);
       },
     );
 
     return () => {
       soundRef.current?.unloadAsync();
-      window.clearTimeout(timer.current);
     };
-  }, []);
-
-  const setAlarm = React.useCallback(() => {
-    const endTime = dayjs().add(Alarm.duration, 'ms');
-    Alarm.setEndTime(endTime);
-
-    setLeft(endTime.fromNow());
-    Alarm.setStatus(AlarmStatus.ticking);
-
-    if (timer.current) {
-      window.clearTimeout(timer.current);
-    }
-
-    timer.current = window.setTimeout(() => {
-      Alarm.setStatus(AlarmStatus.ringing);
-      soundRef.current?.playAsync();
-    }, Alarm.duration);
-  }, []);
-
-  const stopAlarm = React.useCallback(() => {
-    Alarm.setEndTime(null);
-    setLeft(undefined);
-    window.clearTimeout(timer.current);
-    dismissAlarm();
-  }, []);
-
-  const dismissAlarm = React.useCallback(() => {
-    Alarm.setEndTime(null);
-    setLeft(undefined);
-    stopSound();
   }, []);
 
   async function stopSound() {
     await soundRef.current?.stopAsync();
-    Alarm.setStatus(AlarmStatus.waiting);
   }
 
-  const button = React.useMemo(() => {
-    switch (Alarm.status) {
-      case AlarmStatus.waiting:
-        return (
-          <Button
-            btnText="Sleep"
-            onClick={setAlarm}
-            icon={<MaterialCommunityIcons name="sleep" size={30} />}
-          />
-        );
-      case AlarmStatus.ticking:
-        return (
-          <Button
-            btnText="Stop"
-            onClick={stopAlarm}
-            icon={
-              <MaterialCommunityIcons name="stop-circle-outline" size={30} />
-            }
-          />
-        );
-      case AlarmStatus.ringing:
-        return (
-          <Button
-            btnText="Dismiss"
-            onClick={dismissAlarm}
-            icon={<MaterialCommunityIcons name="close" size={30} />}
-          />
-        );
-    }
-  }, [Alarm.status]);
+  async function playSound() {
+    await soundRef.current?.playAsync();
+  }
 
-  React.useEffect(() => {
-    const sub = countdown$.subscribe(() => {
-      const timeLeft = endTime ? endTime.fromNow() : '';
-      console.log({ timeLeft });
-      setLeft(timeLeft);
-    });
-
-    return () => sub.unsubscribe();
-  }, []);
+  if (loading) {
+    return <RNButton title="" loading />;
+  }
 
   return (
     <View style={styles.container}>
       <Top />
-      <Bottom />
-    </View>
-  );
-
-  return (
-    <View style={styles.container}>
-      {endTime && left ? (
-        <View style={styles.container}>
-          <Text style={styles.title}>Shh, waking {left}</Text>
-        </View>
-      ) : null}
 
       <View style={styles.separator}></View>
 
-      <View style={styles.container}>{button}</View>
+      <Bottom stopSound={stopSound} playSound={playSound} />
     </View>
   );
-});
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -145,5 +64,6 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+    backgroundColor: 'white',
   },
 });
