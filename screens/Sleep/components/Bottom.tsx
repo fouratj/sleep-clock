0ef773e918d785b97b1276146c9c3d03f65d9106
Fourat Janabi <fouratj@gmail.com>
2021-01-10 17:30:39 +1100

    Add Bottom comp

--- screens/Sleep/components/Bottom.tsx
+++ screens/Sleep/components/Bottom.tsx
@@ -7,13 +7,85 @@ import { interval } from 'rxjs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import { Text, View } from '../../../components/Themed';
+import Button from '../../../components/Button';
+import Alarm, { AlarmStatus } from '../../../state/alarm';
 
-export default observer(() => {
-  return (
-    <View style={styles.container}>
-      <Text>Bottom</Text>
-    </View>
-  );
+type Props = {
+  stopSound: () => void;
+  playSound: () => void;
+};
+
+export default observer(({ stopSound, playSound }: Props) => {
+  const timer = React.useRef<number>();
+
+  React.useEffect(() => {
+    return () => window.clearTimeout(timer.current);
+  }, []);
+
+  const setAlarm = React.useCallback(() => {
+    const endTime = dayjs().add(Alarm.duration, 'ms');
+    Alarm.setEndTime(endTime);
+    Alarm.setStatus(AlarmStatus.ticking);
+
+    if (timer.current) {
+      window.clearTimeout(timer.current);
+    }
+
+    timer.current = window.setTimeout(() => {
+      Alarm.setStatus(AlarmStatus.ringing);
+      playSound();
+    }, Alarm.duration);
+  }, [playSound, timer]);
+
+  const stopAlarm = React.useCallback(() => {
+    Alarm.setStatus(AlarmStatus.waiting);
+    Alarm.setEndTime(null);
+    window.clearTimeout(timer.current);
+    dismissAlarm();
+  }, []);
+
+  const dismissAlarm = React.useCallback(() => {
+    Alarm.setStatus(AlarmStatus.waiting);
+    Alarm.setEndTime(null);
+    // setLeft(undefined);
+    stopSound();
+  }, []);
+
+  const button = React.useMemo(() => {
+    switch (Alarm.status) {
+      case AlarmStatus.waiting:
+        return (
+          <Button
+            btnText="Sleep"
+            onClick={setAlarm}
+            icon={<MaterialCommunityIcons name="sleep" size={30} />}
+            key="sleep"
+          />
+        );
+      case AlarmStatus.ticking:
+        return (
+          <Button
+            btnText="Stop"
+            onClick={stopAlarm}
+            icon={
+              <MaterialCommunityIcons name="stop-circle-outline" size={30} />
+            }
+            key="stop"
+          />
+        );
+      case AlarmStatus.ringing:
+        return (
+          <Button
+            btnText="Dismiss"
+            onClick={dismissAlarm}
+            icon={<MaterialCommunityIcons name="close" size={30} />}
+            key="dismiss"
+          />
+        );
+    }
+  }, [Alarm.status]);
+
+  return <View style={styles.container}>{button}</View>;
 });
 
 const styles = StyleSheet.create({
@@ -21,14 +93,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'red',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
